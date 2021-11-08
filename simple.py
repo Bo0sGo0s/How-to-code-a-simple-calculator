6baa6f46828e7a963ca78903d0ffb07071e0f4bc
Bo0sGo0s <82037518+Bo0sGo0s@users.noreply.github.com>
2021-11-08 15:02:50 -0500

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,21 @@
+choice = float(input("Enter a number: "))
+print(choice)
+num = choice
+choice1 = float(input("Enter a number: "))
+print(choice1)
+num1 = choice1
+s = float(input("Enter a number: "))
+print(s)
+if s == 1:
+	if s == 1:
+		res = num+num1
+		print("Addition Result = ", res)
+elif s == 2:
+	res = num-num1
+	print("Subtraction Result = ", res)
+elif s == 3:
+	res = num*num1
+	print("Multiplication Result = ", res)
+elif s == 4:
+	res = num/num1
+	print("Division Result = ", res)
